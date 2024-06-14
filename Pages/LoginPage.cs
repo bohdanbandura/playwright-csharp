80001035c1f91e51d37031c45911820e526a4563
bohdanbandura <bohdan.bandura@qatestlab.com>
2024-06-14 14:50:15 +0300

    changes

--- Pages/LoginPage.cs
+++ Pages/LoginPage.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.Playwright;
 using PlaywrighNunit.Locators;
-
+using PlaywrighNunit.Helpers;
 
 namespace PlaywrighNunit.Pages
 {
@@ -26,11 +26,24 @@ namespace PlaywrighNunit.Pages
         {
             await _page.GotoAsync(_base_url);
 
+            await _page.ScreenshotAsync(new() { Path = "../../../screenshots/screenshot1.png" });
+
             await _page.FillAsync(_login_page_locators.userName, login);
+
+            await _page.ScreenshotAsync(new() { Path = "../../../screenshots/screenshot2.png" });
 
             await _page.FillAsync(_login_page_locators.password, _password);
 
+            await _page.ScreenshotAsync(new() { Path = "../../../screenshots/screenshot3.png" });
+
             await _page.ClickAsync(_login_page_locators.loginButton);
+
+            await _page.ScreenshotAsync(new() { Path = "../../../screenshots/screenshot4.png" });
+
+            ImageComparer.CompareImages("../../../expectedScreenshots/expectedScreenshot1.png", "../../../screenshots/screenshot1.png", "../../../diffScreenshots/diffScreenshot1.png");
+            ImageComparer.CompareImages("../../../expectedScreenshots/expectedScreenshot2.png", "../../../screenshots/screenshot2.png", "../../../diffScreenshots/diffScreenshot2.png");
+            ImageComparer.CompareImages("../../../expectedScreenshots/expectedScreenshot3.png", "../../../screenshots/screenshot3.png", "../../../diffScreenshots/diffScreenshot3.png");
+            ImageComparer.CompareImages("../../../expectedScreenshots/expectedScreenshot4.png", "../../../screenshots/screenshot4.png", "../../../diffScreenshots/diffScreenshot4.png");
         }
     }
 }
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -3,147 +3,115 @@ using PlaywrighNunit.Locators;
 using Microsoft.Playwright.NUnit;
 using NUnit.Framework.Legacy;
 using PlaywrighNunit.Pages;
-using PlaywrighNunit.Helpers;
 
 namespace PlaywrighNunit
 {
     public class PlaywrighNunitTests : PageTest
     {
-        // private LoginPage _loginPage;
-        // private CartPage _cartPage;
-        // private ProductsPage _productsPage;
-        // private readonly LoginPageLocators _login_page_locators = new();
-        // private readonly ProductsPageLocators _products_page_locators = new();
-        // private readonly CartPageLocators _cart_page_locators = new();
-        // private readonly string _locked_user_login = "locked_out_user";
+        private LoginPage _loginPage;
+        private CartPage _cartPage;
+        private ProductsPage _productsPage;
+        private readonly LoginPageLocators _login_page_locators = new();
+        private readonly ProductsPageLocators _products_page_locators = new();
+        private readonly CartPageLocators _cart_page_locators = new();
+        private readonly string _locked_user_login = "locked_out_user";
 
-        // public override BrowserNewContextOptions ContextOptions()
-        // {
-        //     return new BrowserNewContextOptions()
-        //     {
-        //         RecordVideoDir = "../../../videos/",
-        //         RecordVideoSize = new RecordVideoSize
-        //         {
-        //             Height = 720,
-        //             Width = 1280
-        //         }
-        //     };
-        // }
+        public override BrowserNewContextOptions ContextOptions()
+        {
+            return new BrowserNewContextOptions()
+            {
+                RecordVideoDir = "../../../videos/",
+                RecordVideoSize = new RecordVideoSize
+                {
+                    Height = 720,
+                    Width = 1280
+                }
+            };
+        }
 
-        // [SetUp]
-        // public async Task Setup()
-        // {
-        //     await Context.Tracing.StartAsync(new()
-        //     {
-        //         Title = TestContext.CurrentContext.Test.ClassName + "." + TestContext.CurrentContext.Test.Name,
-        //         Screenshots = true,
-        //         Snapshots = true,
-        //         Sources = true
-        //     });
+        [SetUp]
+        public async Task Setup()
+        {
+            await Context.Tracing.StartAsync(new()
+            {
+                Title = TestContext.CurrentContext.Test.ClassName + "." + TestContext.CurrentContext.Test.Name,
+                Screenshots = true,
+                Snapshots = true,
+                Sources = true
+            });
 
-        //     _loginPage = new LoginPage(Page);
-        //     _cartPage = new CartPage(Page);
-        //     _productsPage = new ProductsPage(Page);
-        // }
+            _loginPage = new LoginPage(Page);
+            _cartPage = new CartPage(Page);
+            _productsPage = new ProductsPage(Page);
+        }
 
-        // [TearDown]
-        // public async Task TearDown()
-        // {
-        //     await Context.Tracing.StopAsync(new() 
-        //     { 
-        //         Path = Path.Combine("../../../playwright-traces", $"{TestContext.CurrentContext.Test.ClassName}.{TestContext.CurrentContext.Test.Name}.zip")
-        //     });
-        // }
-
-        // [Test]
-        // public async Task AddProductToTheCart()
-        // {
-        //     await _loginPage.Login();
-
-        //     var product = Page.Locator(_products_page_locators.productBlock, new PageLocatorOptions { HasText = "Sauce Labs Backpack" });
-
-        //     var addToCart = product.Locator(_products_page_locators.addToCart);
-
-        //     await Expect(addToCart).ToBeEnabledAsync();
-
-        //     await addToCart.ClickAsync();
-
-        //     var cartBadge = Page.Locator(_products_page_locators.cartBadge);
-
-        //     await Expect(cartBadge).ToBeVisibleAsync();
-
-        //     await Expect(cartBadge).ToHaveTextAsync("1");
-        // }
-
-        // [Test]
-        // public async Task DeleteProductFromTheCart()
-        // {
-        //     await this.AddProductToTheCart();
-
-        //     await Page.Locator(_products_page_locators.cart).ClickAsync();
-
-        //     await Page.Locator(_cart_page_locators.removeBtn).ClickAsync();
-
-        //     var removedItem = Page.Locator(_cart_page_locators.removedItem);
-
-        //     await Expect(removedItem).ToBeEmptyAsync();
-        // }
-
-        // [Test]
-        // public async Task SortingProducts()
-        // {
-        //     await _loginPage.Login();
-
-        //     string[] productNamesAZ = await _productsPage.GetProducts();
-
-        //     var sortingDropdown = Page.Locator(_products_page_locators.sortingDropDown);
-
-        //     await sortingDropdown.SelectOptionAsync(new[] { "Name (Z to A)" });
-
-        //     string[] productNamesZA = await _productsPage.GetProducts();
-
-        //     Assert.That(productNamesZA, Is.EqualTo(productNamesAZ.Reverse()).AsCollection);
-        // }
-
-        // [Test]
-        // public async Task CheckLockedUser()
-        // {
-        //     await _loginPage.Login(_locked_user_login);
-
-        //     var errorMessage = Page.Locator(_login_page_locators.errorMessage);
-
-        //     await Expect(errorMessage).ToHaveTextAsync("Epic sadface: Sorry, this user has been locked out.");
-        // }
+        [TearDown]
+        public async Task TearDown()
+        {
+            await Context.Tracing.StopAsync(new() 
+            { 
+                Path = Path.Combine("../../../playwright-traces", $"{TestContext.CurrentContext.Test.ClassName}.{TestContext.CurrentContext.Test.Name}.zip")
+            });
+        }
 
         [Test]
-        public async Task CompareScreenshots()
+        public async Task AddProductToTheCart()
         {
-            await Page.GotoAsync("https://www.google.com");
+            await _loginPage.Login();
 
-            await Page.ScreenshotAsync(new() { Path = "../../../screenshots/screenshot1.png" });
+            var product = Page.Locator(_products_page_locators.productBlock, new PageLocatorOptions { HasText = "Sauce Labs Backpack" });
 
-            await Page.Locator("textarea[name=\"q\"]").FillAsync("qqqq");
+            var addToCart = product.Locator(_products_page_locators.addToCart);
 
-            await Page.ScreenshotAsync(new() { Path = "../../../screenshots/screenshot2.png" });
+            await Expect(addToCart).ToBeEnabledAsync();
 
-            await Page.PressAsync("textarea[name=\"q\"]", "Enter");
+            await addToCart.ClickAsync();
 
-            await Page.ScreenshotAsync(new() { Path = "../../../screenshots/screenshot3.png" });
+            var cartBadge = Page.Locator(_products_page_locators.cartBadge);
 
-            await Page.Locator("h3", new PageLocatorOptions { HasText = "QQQQ: Definition, Composition, and Current Ticker" }).First.ScrollIntoViewIfNeededAsync();
-            await Page.Locator("h3", new PageLocatorOptions { HasText = "QQQQ: Definition, Composition, and Current Ticker" }).First.ClickAsync();
+            await Expect(cartBadge).ToBeVisibleAsync();
 
-            await Page.ScreenshotAsync(new() { Path = "../../../screenshots/screenshot4.png" });
+            await Expect(cartBadge).ToHaveTextAsync("1");
+        }
 
-            await Page.Locator("li[class=\"header-nav__list-item\"]", new PageLocatorOptions { HasText = "News"}).ClickAsync();
+        [Test]
+        public async Task DeleteProductFromTheCart()
+        {
+            await this.AddProductToTheCart();
 
-            await Page.ScreenshotAsync(new() { Path = "../../../screenshots/screenshot5.png" });
+            await Page.Locator(_products_page_locators.cart).ClickAsync();
 
-            ImageComparer.CompareImages("../../../expectedScreenshots/expectedScreenshot1.png", "../../../screenshots/screenshot1.png", "../../../diffScreenshots/diffScreenshot1.png");
-            ImageComparer.CompareImages("../../../expectedScreenshots/expectedScreenshot2.png", "../../../screenshots/screenshot2.png", "../../../diffScreenshots/diffScreenshot2.png");
-            ImageComparer.CompareImages("../../../expectedScreenshots/expectedScreenshot3.png", "../../../screenshots/screenshot3.png", "../../../diffScreenshots/diffScreenshot3.png");
-            ImageComparer.CompareImages("../../../expectedScreenshots/expectedScreenshot4.png", "../../../screenshots/screenshot4.png", "../../../diffScreenshots/diffScreenshot4.png");
-            ImageComparer.CompareImages("../../../expectedScreenshots/expectedScreenshot5.png", "../../../screenshots/screenshot5.png", "../../../diffScreenshots/diffScreenshot5.png");
+            await Page.Locator(_cart_page_locators.removeBtn).ClickAsync();
+
+            var removedItem = Page.Locator(_cart_page_locators.removedItem);
+
+            await Expect(removedItem).ToBeEmptyAsync();
+        }
+
+        [Test]
+        public async Task SortingProducts()
+        {
+            await _loginPage.Login();
+
+            string[] productNamesAZ = await _productsPage.GetProducts();
+
+            var sortingDropdown = Page.Locator(_products_page_locators.sortingDropDown);
+
+            await sortingDropdown.SelectOptionAsync(new[] { "Name (Z to A)" });
+
+            string[] productNamesZA = await _productsPage.GetProducts();
+
+            Assert.That(productNamesZA, Is.EqualTo(productNamesAZ.Reverse()).AsCollection);
+        }
+
+        [Test]
+        public async Task CheckLockedUser()
+        {
+            await _loginPage.Login(_locked_user_login);
+
+            var errorMessage = Page.Locator(_login_page_locators.errorMessage);
+
+            await Expect(errorMessage).ToHaveTextAsync("Epic sadface: Sorry, this user has been locked out.");
         }
     }
 }
